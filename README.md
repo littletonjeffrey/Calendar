c2bf30d8139314893e1fbaec3563aa56f4f1607d
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-07-06 09:02:31 -0500

    framework done

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Calendar
+vincent caruso via https://stackoverflow.com/questions/62462599/how-can-i-change-elements-style-based-on-the-time-of-day
--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-var currentDayEl= document.getElementByID('currentDay');
-currentDayEl=moment().format("DD/MM/YYYY");
+var currentDay = moment();
+$("#currentDay").text(currentDay.format("MMM Do, YYYY"));
+// grab current hour
+let currentHour = moment().format('H');
+// grab all them text blocks
+var textBlocks = $(".form-control");
+// Loop through each text block and change background based on what hour it is using moment
+for (var i = 0; i < textBlocks.length; i++) {
+    var textBlocksID = textBlocks[i].id;
+    var change = document.getElementById(textBlocks[i].id);
 
-let currentDay = document.createElement("p");
-document.body.header.p.appendChild(currentDay);
+    // $(textBlocksID[i].id).removeClass('.present .past .future');
+    if (textBlocksID < currentHour) {
+        $(change).removeClass('.present .past .future');
+        $(change).addClass('.past');
+        
+    } else if (textBlocksID > currentHour) {
+        $(change).removeClass('.present .past .future');
+        $(change).addClass('.future');
+    } else {
+        $(change).removeClass('.present .past .future');
+        $(change).addClass('.present');
+    } console.log($(textBlocksID[i]))
+}
