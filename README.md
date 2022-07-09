4d513c717b8f57a60e7dd38443b8dcf5c45fde76
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-07-09 18:10:10 -0500

    Final

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Calendar
-vincent caruso via https://stackoverflow.com/questions/62462599/how-can-i-change-elements-style-based-on-the-time-of-day
+# Bootcamp Challenge: Hourly Task List
+
+## Project Description
+
+This project came with completed HTML and CSS.  The client wanted a daily schedule for the working hours of their day that would change color dependent on the current hour, as well as the function to save tasks with a button and have them stay on the page even if refreshed.  
+
+## Finished Project Preview
+
+![Picture of Completed Project](./Assets/calendar.png)
+
+[Deployed Project](https://littletonjeffrey.github.io/Calendar/)
+
+[Github Repo Link](https://github.com/littletonjeffrey/Calendar)
+
+## Thoughts Behind The Code
+
+I utilized Bootstrap to build the UI.  I merged the save button into the time indicator for each row to allow for more space.  Utilized Vincent Caruso's advice on how to get the input fields to change color.  
+
+Nine AM was still not working properly until I realized that since moment was tracking time in military time formatted for HH, I had to put a '0' in front of the '9'.  
+
+Had trouble getting items out of local storage and back into their proper place, but my tutor, Eric Sayer, helped me with that.  
+
+Happy to have a functioning program that meets all the client's requirements.
+
+
+## Contributors
+
+- Computational Wizard: [Eric Sayer](https://github.com/sayex)
+- Software Ninjaneer: Vincent Caruso via[Stackoverflow.com](https://stackoverflow.com/questions/62462599/how-can-i-change-elements-style-based-on-the-time-of-day)
--- script.js
+++ script.js
@@ -2,23 +2,55 @@ var currentDay = moment();
 $("#currentDay").text(currentDay.format("MMM Do, YYYY"));
 // grab current hour
 let currentHour = moment().format('H');
+console.log(currentHour)
 // grab all them text blocks
 var textBlocks = $(".form-control");
+//variables for local storage
+var save4Later = document.querySelector(".input-group");
+var task = document.querySelector('.form-control');
+var button = $('.saveBtn');
+
+
+
+
+
+var localStorageSave = function() {
+    var userInput = $(this).parent().siblings('input').val()
+    var id = $(this).parent().siblings('input').attr('id')
+    console.log(id)
+    localStorage.setItem(id, userInput);
+}
+
+button.on('click', localStorageSave);
+
+
+
+
 // Loop through each text block and change background based on what hour it is using moment
 for (var i = 0; i < textBlocks.length; i++) {
     var textBlocksID = textBlocks[i].id;
     var change = document.getElementById(textBlocks[i].id);
-
+// console.log(textBlocks[i])
     // $(textBlocksID[i].id).removeClass('.present .past .future');
     if (textBlocksID < currentHour) {
-        $(change).removeClass('.present .past .future');
-        $(change).addClass('.past');
+        $(textBlocks[i]).removeClass('present past future');
+        $(textBlocks[i]).addClass('past');
         
     } else if (textBlocksID > currentHour) {
-        $(change).removeClass('.present .past .future');
-        $(change).addClass('.future');
+        $(textBlocks[i]).removeClass('present past future');
+        $(textBlocks[i]).addClass('future');
     } else {
-        $(change).removeClass('.present .past .future');
-        $(change).addClass('.present');
-    } console.log($(textBlocksID[i]))
+        $(textBlocks[i]).removeClass('present past future');
+        $(textBlocks[i]).addClass('present');
+    // } console.log($(textBlocksID[i]))
+    }}
+    
+
+
+for (var j = 9; j <= 17; j++) {
+    if (j===9){
+    $('#0'+j).val(localStorage.getItem('0'+j))
+}else{
+    $('#'+j).val(localStorage.getItem(j))
+}
 }
